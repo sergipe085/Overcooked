9e731ee1d7e7f2d5db9f15388d5f1df6a0efbe66
Sérgio <71034184+serjolas1@users.noreply.github.com>
2023-02-21 17:41:08 -0300

    transform movement (with collision detection)

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,21 +4,46 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    [SerializeField] private Rigidbody rig = null;
     [SerializeField] private float moveSpeed = 5.0f;
 
     private Vector3 moveDir = Vector2.zero;
 
     private void Update() {
-        Vector2 inputVector = GameInput.Instance.GetMovementVectorNormalized();
-        moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
+        MoveTransform();
 
         float rotateSpeed = 10.0f;
         transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotateSpeed);
     }
 
-    private void FixedUpdate() {
-        rig.velocity = moveDir * moveSpeed;
+    private void MoveTransform() {
+        Vector2 inputVector = GameInput.Instance.GetMovementVectorNormalized();
+        moveDir = new Vector3(inputVector.x, 0f, inputVector.y).normalized;
+        
+        float playerHeight = 0.7f;
+        float playerRadius = 0.5f;
+        float moveDistance = moveSpeed * Time.deltaTime;
+        bool canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDir, moveDistance);
+
+        if (!canMove) {
+            Vector3 moveDirX = new Vector3(moveDir.x, 0, 0).normalized;
+            canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirX, moveDistance);
+
+            if (canMove) {
+                moveDir = moveDirX;
+            }
+            else {
+                Vector3 moveDirZ = new Vector3(0, 0, moveDir.z).normalized;
+                canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirZ, moveDistance);
+
+                if (canMove) {
+                    moveDir = moveDirZ;
+                }
+            }
+        }
+
+        if (canMove) {
+            transform.position += moveDir * moveDistance;
+        }
     }
 
     public bool IsWalking() {
