f60f8e95407fd8c149447a6e40123372bcffe888
Sérgio <71034184+serjolas1@users.noreply.github.com>
2023-02-22 14:35:37 -0300

    spawniing objects

--- Assets/Scripts/ClearCounter.cs
+++ Assets/Scripts/ClearCounter.cs
@@ -4,7 +4,11 @@ using UnityEngine;
 
 public class ClearCounter : MonoBehaviour
 {
+    [SerializeField] private GameObject tomatoPrefab = null;
+    [SerializeField] private Transform counterTopPoint = null;
+
     public void Interact() {
         Debug.Log("Interact with " + transform.name);
+        Instantiate(tomatoPrefab, counterTopPoint);
     }
 }
